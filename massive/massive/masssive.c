3f41c16e4bb2bcf2fa9cf56b9d85a6172c270787
Dmitry Alekseevich <opolindmitry@gmail.com>
2022-09-29 17:01:41 +0300

    добавил алгоритм возвеления в степень кратных чисел

--- massive/massive/masssive.c
+++ massive/massive/masssive.c
@@ -2,22 +2,25 @@
 #include<locale.h>
 #include<stdlib.h>
 #include<time.h>
+#include<math.h>
 int main() {
 	setlocale(LC_ALL, "rus");
-	int size, num, stepen;
+	int size, num;
+	float stepen;
 	srand(time(0));
 	printf("Введите размер массива (до 10000): ");
 	scanf_s("%d", &size);
 	int mass[10000];
 	for (int i = 0; i < size; i++) {
 		mass[i] = -10 + rand() % 21;
+		printf("%d ", mass[i]);
 	}
 	printf("Введите число, кратные которому числа в массиве будут возведены в заданную вами степень(от 2 до 5) : \n");
 	printf("Число: ");
 	scanf_s("%d", &num);
 	printf("Степень: ");
-	scanf_s("%d", &stepen);
-	for (int i = size, i > 0, i--) {
+	scanf_s("%f", &stepen);
+	for (int i = size - 1; i >= 0; i--) {
 		if (mass[i] % num == 0) {
 			mass[i] = pow(mass[i], stepen);
 		}
